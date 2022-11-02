1ca8a0b6340b6b50cb1eec98f3379a541cfe8d5e
[Bryan Dalton] <[bryantdalton19@gmail.com]>
2022-11-02 13:19:29 -0400

    delete attempt and formating

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const util = require('util');
-const uuid = require('./helpers/uuid');
-
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
+const uuid = require("./helpers/uuid");
 
 const PORT = 3001;
 const app = express();
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-
+app.use(express.static("public"));
 
 const readFromFile = util.promisify(fs.readFile);
 
@@ -22,57 +19,55 @@ const writeToFile = (destination, content) =>
   );
 
 const readAndAppend = (content, file) => {
-    fs.readFile(file, 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-      } else {
-        const parsedData = JSON.parse(data);
-        parsedData.push(content);
-        writeToFile(file, parsedData);
-      }
-    });
-  };
-
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'/public/index.html'))
-    console.info(`${req.method} Request recieved from index.html`)
-  })
-  
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname,'/public/notes.html'))
-    console.info(`${req.method} Request recieved from notes.html`)
-  })
-
-app.get('/api/notes', (req, res) => {
-    console.info(`${req.method} request received for notes`);
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  fs.readFile(file, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
   });
+};
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+  console.info(`${req.method} Request recieved from index.html`);
+});
 
-app.post('/api/notes', (req, res) => {
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+  console.info(`${req.method} Request recieved from notes.html`);
+});
+
+app.get("/api/notes", (req, res) => {
+  console.info(`${req.method} request received for notes`);
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+app.post("/api/notes", (req, res) => {
   console.info(`${req.method} Post Successful`);
-  console.log(`${req.body}`)
-  const {title, text} = req.body;
+  console.log(`${req.body}`);
+  const { title, text } = req.body;
 
   if (req.body) {
     const newNote = {
       title,
       text,
-      id: uuid()
+      id: uuid(),
     };
 
-    readAndAppend(newNote, './db/db.json');
+    readAndAppend(newNote, "./db/db.json");
     res.json(`Note Added!`);
   } else {
-    res.error('Error in saving note');
+    res.error("Error in saving note");
   }
-  })
+});
 
+app.delete(`/api/notes/:id`, (req, res) => {
+  res.send(delete Object);
+});
 
 app.listen(PORT, () =>
-    console.log(`Example app listening at http://localhost:${PORT}`)
-  );
-
-  //load a specific note, uuid?
-  //delete notes
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
